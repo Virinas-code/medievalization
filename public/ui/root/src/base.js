7589b9c94b8aa6d7881453cead20a72f1fc39e21
Virinas-code <alexcode228@gmail.com>
2023-04-10 14:19:03 +0200

    feat(web): Add icon!

--- public/ui/root/src/base.js
+++ public/ui/root/src/base.js
@@ -1,6 +1,9 @@
 import { ready } from "../../components/src/scbo.js";
+function changeLanguage(language) {
+    fetch("/api/i18n/" + language).then(() => { location.reload(); });
+}
 function loadSCBO() {
-    window.Components["SCBO"][0].setHandle(console.log);
+    window.Components["SCBO"][0].setHandle(changeLanguage);
 }
 window.addEventListener(ready.type, loadSCBO);
 //# sourceMappingURL=base.js.map
--- ui/root/src/base.ts
+++ ui/root/src/base.ts
@@ -1,8 +1,12 @@
 export {};
 import { SmallCombo, ready } from "../../components/src/scbo.js";
 
+function changeLanguage(language: string): void {
+    fetch("/api/i18n/" + language).then(() => {location.reload();});
+}
+
 function loadSCBO(): void {
-    (window.Components["SCBO"][0] as SmallCombo).setHandle(console.log);
+    (window.Components["SCBO"][0] as SmallCombo).setHandle(changeLanguage);
 }
 
 window.addEventListener(ready.type, loadSCBO);
